1a9840b67c979e01bbe40d46207bc406a2527e8c
Anshuman <anshu.av97@gmail.com>
2017-12-30 02:40:03 +0530

    Add function name, docs, proper var names

--- download.js
+++ download.js
@@ -1,15 +1,19 @@
-(function() {
+function injectButtons() {
 
+	// Grab header elements in each post
 	headers = document.querySelectorAll('header');
 
+	// Grab images appearing on the page
 	images = document.querySelectorAll('img');
 
+	// Iterate through the headers and inject a button to download corresponding image
 	for(let i = 0; i<headers.length; i++) {
-		let instanshu = document.createElement('a');
-		instanshu.innerHTML = `<a download href=${images[(2*i)+1].src}><button>Download</button>`;
+		let dlbutton = document.createElement('a');
+		dlbutton.innerHTML = `<a download href=${images[(2*i)+1].src}><button>Download</button>`;
 		if(headers[i].querySelectorAll('a').length < 3) {
-			headers[i].appendChild(instanshu);
+			headers[i].appendChild(dlbutton);
 		}
 	}
+}
 
-})();
+injectButtons();
