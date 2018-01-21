7264fc24b7819a2f9fa8dd8f76cda8864cfa5a6e
Aashutosh Rathi <aashutoshrathi@gmail.com>
2018-01-21 18:12:29 +0530

    Fixes #12 : Load CSS once

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+chrome.tabs.insertCSS(null, {file: "insta-down-css.css"});
+chrome.tabs.insertCSS(null, {file: "fa5.css"});
+
 chrome.tabs.onUpdated.addListener(function() {
   chrome.tabs.executeScript(null, { file: "download.js" });
 });
