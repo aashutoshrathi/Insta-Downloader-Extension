3d7e9ef5fd78486701faac6ba9799793d40e1bb0
Double-A-92 <amato.amedeo@gmail.com>
2018-10-02 13:34:41 +0200

    format all code with "Prettier" (#25)
    * format all code with "Prettier"
    
    There didn't seem to be any consistent formatting going on, so I applied some.
    Used Prettier@1.14.2 in VSCode for that.
    
    * add newline at end of files
    
    I guess fulfill POSIX stardards

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.tabs.insertCSS(null, {file: "insta-down-css.css"});
+chrome.tabs.insertCSS(null, { file: "insta-down-css.css" });
 
 chrome.tabs.onUpdated.addListener(function() {
   chrome.tabs.executeScript(null, { file: "download.js" });
