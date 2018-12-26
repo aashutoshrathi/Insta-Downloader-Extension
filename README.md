277f5976d97ad42921f87b377a932e3d4f2c7cee
Gautam krishna R <gautam.r@attinadsoftware.com>
2018-12-26 19:06:56 +0530

    merging master to firefox # Conflicts: #	README.md #	background.js #	manifest.json #	popup.html #	style.css

--- README.md
+++ README.md
@@ -1,34 +1,63 @@
-# Instagram Downloader [![OpenCode](https://img.shields.io/badge/Open-Code-ff6a00.svg?style=flat-square)](https://opencode18.github.io)
+<p align="center"><img src="img/icon-128x128.png" align="center" width="128"><p>
 
-[<img src="img/icon-128x128.png" align="right" width="100">](https://github.com/aashutoshrathi/Insta-Downloader-Extension)
-
-[![forthebadge](http://forthebadge.com/images/badges/built-with-love.svg)](http://forthebadge.com)
-[![forthebadge](http://forthebadge.com/images/badges/uses-js.svg)](http://forthebadge.com)
-[![forthebadge](http://forthebadge.com/images/badges/makes-people-smile.svg)](http://forthebadge.com)
-
-An extension that injects a button to each post to download the corresponding image/video in the best available quality. :tada:.
-
+<h1 align="center"> Instagram Downloader </h1> 
+<p align="center">
+<a href="https://travis-ci.org/aashutoshrathi/Insta-Downloader-Extension">
+    <img src="https://img.shields.io/travis/aashutoshrathi/Insta-Downloader-Extension/master.svg?style=for-the-badge" align="center">
+</a>
+<a href="https://addons.mozilla.org/en-US/firefox/addon/instagram-media-downloader/">
+    <img src="https://img.shields.io/amo/d/instagram-media-downloader.svg?style=for-the-badge" align="center">
+</a>
+<a href="https://addons.mozilla.org/en-US/firefox/addon/instagram-media-downloader/">
+    <img src="https://img.shields.io/amo/users/instagram-media-downloader.svg?style=for-the-badge" align="center">
+</a>
+<a href="/LICENSE">
+    <img src="https://img.shields.io/github/license/aashutoshrathi/Insta-Downloader-Extension.svg?style=for-the-badge" align="center">
+</a>
+</p>
+<hr>
+An extension that adds a button to each post to download the corresponding image/video in the best available quality. :tada:.
 
 ## In action :movie_camera:
 
 ![Demo](https://media.giphy.com/media/3ohs4rvNk4punuQAta/giphy.gif)
 
-
 ## Installation
 
-### Firefox Users:
+### Firefox Users
 
 [<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Firefox_Logo%2C_2017.svg/2000px-Firefox_Logo%2C_2017.svg.png" align="right" width="90">](https://addons.mozilla.org/en-US/firefox/addon/instagram-media-downloader/)
 
- - Download it from Store: https://addons.mozilla.org/en-US/firefox/addon/instagram-media-downloader/
+- Download it from Store: https://addons.mozilla.org/en-US/firefox/addon/instagram-media-downloader/
 
-   **OR**
-   
-   Check Firefox Development Branch [here](https://github.com/aashutoshrathi/Insta-Downloader-Extension/tree/firefox)
+  **OR**
 
- - **Please leave feedback and ratings** ✍️
- - To test it, go to [Instagram](https://www.instagram.com).
- - Binge Download all posts you like :rocket:.
+  Check Firefox Development Branch [here](https://github.com/aashutoshrathi/Insta-Downloader-Extension/tree/firefox)
 
+- **Please leave feedback and ratings** ✍️
+- To test it, go to [Instagram](https://www.instagram.com).
+- Binge Download all posts you like :rocket:.
+
+### Chrome Users
+
+- Get it locally -
+
+  ```sh
+  $ git clone https://github.com/aashutoshrathi/Insta-Downloader-Extension.git
+  ```
+
+  **OR**
+
+  Download the latest version here: [Instagram Downloader ✨](https://github.com/aashutoshrathi/Insta-Downloader-Extension/archive/master.zip)
+
+- Go to `chrome://extensions/`.
+- Click on Load Unpacked Extension and Open/Select the folder.
+
+* To test it, go to [Instagram](https://www.instagram.com).
+* Binge Download all posts you like :rocket:.
+
+##### Note
+
+If you can't find the button on the top of the page, make sure that Developer mode is enabled [here](https://developer.chrome.com/extensions/faq#faq-dev-01).
 
 <p align="center"> Made from scratch with ❤ by <a href="https://github.com/aashutoshrathi">Aashutosh Rathi</a> and <a href="https://github.com/anshumanv">Anshuman Verma</a> </p>
--- download.js
+++ download.js
@@ -1,148 +1,293 @@
-var link = document.createElement("link");
-link.href = chrome.extension.getURL("insta-down-css.css");
-link.type = "text/css";
-link.rel = "stylesheet";
-document.getElementsByTagName("head")[0].appendChild(link);
+/* eslint no-undef: "error" */
+/* eslint-env browser */
 
-function injectButtons() {
-  buttons = document.querySelectorAll('button');
-  else_profile = false;
-  self_profile = false;
-  explore = false;
-  headings = document.querySelectorAll('h2')
+function escapeHTML (str) {
+  return str.replace(/[&"'<>]/g, m => escapeHTML.replacements[m])
+}
+escapeHTML.replacements = {
+  '&': '&amp;',
+  '"': '&quot;',
+  "'": '&#39;',
+  '<': '&lt;',
+  '>': '&gt;'
+}
 
-  for(var i=0; i<buttons.length; i++) {
-    if(buttons[i].innerText === "Follow" || buttons[i].innerText === "Following") {
-      else_profile = true;
-    }
-    if(buttons[i].innerText === "Edit Profile") {
-      self_profile = true;
+const link = document.createElement('link')
+link.href = chrome.extension.getURL('insta-down-css.css')
+link.type = 'text/css'
+link.rel = 'stylesheet'
+document.getElementsByTagName('head')[0].appendChild(link)
+let i = 0
+
+console.log(
+  '%c Hello Developer!\n Star us here: https://github.com/aashutoshrathi/Insta-Downloader-Extension',
+  'background: #222; color: #bada55'
+)
+const getHTML = function gethtml (url, callback) {
+  // Feature detection
+  if (!window.XMLHttpRequest) return
+
+  // Create new request
+  const xhr = new XMLHttpRequest()
+
+  // Setup callback
+  xhr.onload = function onload () {
+    if (callback && typeof callback === 'function') {
+      callback(this.responseXML)
     }
   }
 
-  for(var i=0; i<headings.length; i++) {
-    if(headings[i].innerText === "Explore") {
-      explore = true;
+  // Get the HTML
+  xhr.open('GET', url)
+  xhr.responseType = 'document'
+  xhr.send()
+}
+
+function injectButtons () {
+  const buttons = document.querySelectorAll('button')
+  let elseProfile = false
+  let selfProfile = false
+  let explore = false
+  const headings = document.querySelectorAll('h2')
+
+  for (i = 0; i < buttons.length; i += 1) {
+    if (
+      document.getElementsByClassName(
+        'glyphsSpriteMore_horizontal__outline__24__grey_9 u-__7'
+      ).length === 1
+    ) {
+      elseProfile = true
+      console.log('%c Some Profile ', 'background: #222; color: #bada55')
+      break
+    }
+    if (buttons[i].innerText === 'Edit Profile') {
+      selfProfile = true
+      console.log('%c Your Profile', 'background: #222; color: #bada55')
+      break
     }
   }
 
+  for (i = 0; i < headings.length; i += 1) {
+    if (headings[i].innerText === 'Explore') {
+      explore = true
+      break
+    }
+  }
 
-	// Grab header elements in each post or profile
-	headers = document.querySelectorAll('header');
-
-	// Grab images appearing on the page
-  images = document.querySelectorAll('img');
+  // Grab header elements in each post or profile
+  const headers = document.querySelectorAll('header')
 
   // Grab images appearing on the page
-  videos = document.querySelectorAll('video');
-  let j = 0;
+  const images = document.querySelectorAll('img')
 
-  if(explore) {
-    for(let i = 3; i<images.length; i++) {
-      let x = images[i].parentElement.parentElement.parentElement.parentElement;
-      let db = document.createElement('a');
-      db.innerHTML = `<a download href=${images[i].src}>
-                  <button class="instanshu-unite instanshu-sm instanshu-success" style="left: 100px; top: 10px;">Download</button>`;
-      if(x.querySelectorAll('button').length < 1) {
-          x.appendChild(db);
+  for (i = 0; i < images.length; i += 1) {
+    // making the image link a downloading link
+    if (!images[i].src.endsWith('&dl=1')) {
+      images[i].src = `${images[i].src}&dl=1`
+    }
+  }
+  // // Grab images appearing on the page
+  const videos = document.querySelectorAll('video')
+  // // making the video link as downloading dlink
+
+  if (videos[0]) {
+    for (i = 0; i < videos.length; i += 1) {
+      if (!videos[i].src.endsWith('&dl=1')) {
+        videos[i].src = `${videos[i].currentSrc}&dl=1`
       }
-      btns = x.querySelectorAll('button');
-      if(btns[0] && btns[0].parentElement.href === document.location.href) {
-        btns[0].parentElement.href = images[i].src;
+      if (!videos[i].poster.endsWith('&dl=1')) {
+        videos[i].poster = `${videos[i].poster}&dl=1`
       }
     }
-
   }
+  let j = 0
 
-  else if(else_profile) {
-    let dlbutton = document.createElement('a');
-    dlbutton.innerHTML = `
-                <a download href=${images[0].src}>
-                <button class="instanshu-unite instanshu-sm instanshu-success">Download DP</button>`;
-      if(headers[0].querySelectorAll('button').length < 3) {
-        headers[0].appendChild(dlbutton);
-      }
+  // Explore Page
+  if (explore) {
+    for (i = 3; i < images.length; i += 1) {
+      const x =
+        images[i].parentElement.parentElement.parentElement.parentElement
 
-    for(let i = 1; i<images.length; i++) {
-      let x = images[i].parentElement.parentElement.parentElement.parentElement;
-      let db = document.createElement('a');
-      db.innerHTML = `<a download href=${images[i].src}>
-                  <button class="instanshu-unite instanshu-sm instanshu-success" style="left: 100px; top: 10px;">Download</button>`;
-      if(x.querySelectorAll('button').length < 1) {
-          x.appendChild(db);
-      }
-      btns = x.querySelectorAll('button');
-      if(btns[0] && btns[0].parentElement.href === document.location.href) {
-        btns[0].parentElement.href = images[i].src;
-      }
-    } 
-  }
+      // For downloading video
+      if (x.getElementsByClassName('Byj2F').length === 1) {
+        // getting video link
+        const y = images[i].parentElement.parentElement.parentElement
 
-  else if (self_profile) {
-    let dlbutton = document.createElement('a');
-    dlbutton.innerHTML = `
-                <a download href=${images[0].src}>
-                <button class="instanshu-unite instanshu-sm instanshu-success">Download DP</button>`;
-    if(headers[0].querySelectorAll('button').length < 4) {
-      headers[0].appendChild(dlbutton);
-    }
+        // get the html content of video link to download video
+        getHTML(y, response => {
+          const video = response.querySelectorAll(
+            "meta[property='og:video:secure_url']"
+          )
+          const db = document.createElement('a')
+          // making the image link a downloading link
+          video[0].content = `${video[0].content}&dl=1`
 
-    for(let i = 1; i<images.length; i++) {
-      let x = images[i].parentElement.parentElement.parentElement.parentElement;
-      let db = document.createElement('a');
-      db.innerHTML = `<a download href=${images[i].src}>
-                  <button class="instanshu-unite instanshu-sm instanshu-success" style="left: 100px; top: 10px;">Download</button>`;
-      if(x.querySelectorAll('button').length < 1) {
-          x.appendChild(db);
-      }
-      btns = x.querySelectorAll('button');
-      if(btns[0] && btns[0].parentElement.href === document.location.href) {
-        btns[0].parentElement.href = images[i].src;
-      }
-    }      
-  }
-
-  else {
-    // Iterate through the headers and inject a button to download corresponding image
-    for(let i = 0; i<headers.length; i++) {
-      let dlbutton = document.createElement('a');
-      let pfbutton = document.createElement('a');
-      dlink = images[(2*i)+1].src;
-      dplink = images[2*i].src;
-      if (images[(2*i) + 1].naturalHeight === 150 || (videos[j] && videos[j].poster === dlink)) {
-        if(videos[j].src) {
-          dlink = videos[j].src;
+          db.innerHTML = `<a download href=${escapeHTML(video[0].content)}>
+                    <button class="instanshu-unite instanshu-sm instanshu-success" style="left: 100px; top: 10px;"> Download Video</button>`
+          if (x.querySelectorAll('button').length < 1) {
+            x.appendChild(db)
+          }
+        })
+      } else {
+        const db = document.createElement('a')
+        db.innerHTML = `<a download href=${escapeHTML(images[i].src)}>
+                  <button class="instanshu-unite instanshu-sm instanshu-success" style="left: 100px; top: 10px;">Download</button>`
+        if (x.querySelectorAll('button').length < 1) {
+          x.appendChild(db)
         }
-        j++;
+        const btns = x.querySelectorAll('button')
+        if (btns[0] && btns[0].parentElement.href === document.location.href) {
+          btns[0].parentElement.href = images[i].src
+        }
       }
+    }
+  } else if (elseProfile) {
+    // Someone else's profile
+    const dlbutton = document.createElement('a')
+    dlbutton.innerHTML = `
+                <a download href=${escapeHTML(images[0].src)}>
+                <button class="instanshu-unite instanshu-sm instanshu-success">Download DP</button>
+                 `
+    if (headers[0].querySelectorAll('button').length < 1) {
+      headers[0].appendChild(dlbutton)
+    }
+
+    for (i = 1; i < images.length; i += 1) {
+      const x =
+        images[i].parentElement.parentElement.parentElement.parentElement
+
+      if (x.getElementsByClassName('Byj2F').length === 1) {
+        const y = images[i].parentElement.parentElement.parentElement
+        getHTML(y, response => {
+          const video = response.querySelectorAll(
+            "meta[property='og:video:secure_url']"
+          )
+          const db = document.createElement('a')
+          video[0].content = `${video[0].content}&dl=1`
+
+          db.innerHTML = `<a download href=${escapeHTML(video[0].content)}>
+                    <button class="instanshu-unite instanshu-sm instanshu-success" style="left: 100px; top: 10px;"> Download Video</button>`
+          if (x.querySelectorAll('button').length < 1) {
+            x.appendChild(db)
+          }
+        })
+      } else {
+        const db = document.createElement('a')
+        if (images[i].className === '_6q-tv' && images[i].height === 50) {
+          db.innerHTML = `<a download href=${escapeHTML(images[i].src)}>
+                  <button class="instanshu-unite instanshu-sm instanshu-success" style="left: -160px; top: -9px;">Download</button>`
+        } else {
+          db.innerHTML = `<a download href=${escapeHTML(images[i].src)}>
+                  <button class="instanshu-unite instanshu-sm instanshu-success" style="left: 100px; top: 10px;">Download</button>`
+        }
+        if (x.querySelectorAll('button').length < 1) {
+          x.appendChild(db)
+        }
+        const btns = x.querySelectorAll('button')
+        if (btns[0] && btns[0].parentElement.href === document.location.href) {
+          btns[0].parentElement.href = images[i].src
+        }
+      }
+    }
+  } else if (selfProfile) {
+    const dlbutton = document.createElement('a')
+    dlbutton.innerHTML = `
+                <a download href=${escapeHTML(images[0].src)}>
+                <button class="instanshu-unite instanshu-sm instanshu-success">Download DP</button>`
+    if (headers[0].querySelectorAll('button').length < 4) {
+      headers[0].appendChild(dlbutton)
+    }
+
+    for (i = 1; i < images.length; i += 1) {
+      const x =
+        images[i].parentElement.parentElement.parentElement.parentElement
+      if (x.getElementsByClassName('Byj2F').length === 1) {
+        const y = images[i].parentElement.parentElement.parentElement
+        getHTML(y, response => {
+          const video = response.querySelectorAll(
+            "meta[property='og:video:secure_url']"
+          )
+          const db = document.createElement('a')
+          video[0].content = `${video[0].content}&dl=1`
+
+          db.innerHTML = `<a download href=${escapeHTML(video[0].content)}>
+                    <button class="instanshu-unite instanshu-sm instanshu-success" style="left: 100px; top: 10px;"> Download Video</button>`
+          if (x.querySelectorAll('button').length < 1) {
+            x.appendChild(db)
+          }
+        })
+      } else {
+        const db = document.createElement('a')
+        db.innerHTML = `<a download href=${escapeHTML(images[i].src)}>
+                  <button class="instanshu-unite instanshu-sm instanshu-success" style="left: 100px; top: 10px;">Download</button>`
+        if (x.querySelectorAll('button').length < 1) {
+          x.appendChild(db)
+        }
+        const btns = x.querySelectorAll('button')
+        if (btns[0] && btns[0].parentElement.href === document.location.href) {
+          btns[0].parentElement.href = images[i].src
+        }
+      }
+    }
+  } else {
+    // Handle main page here
+    // Iterate through the headers and inject a button to download corresponding image
+    console.log('%c Main Page', 'background: #222; color: #bada55')
+    for (i = 0; i < headers.length; i += 1) {
+      const dlbutton = document.createElement('a')
+      const pfbutton = document.createElement('a')
+      const buttonsParent = document.createElement('div')
+
+      buttonsParent.className = 'download-buttons'
+      let dlink = images[2 * i + 1].src
+      let dplink = images[2 * i].src
+      let label = 'Download'
+      if (videos[j] && videos[j].poster === dlink) {
+        /**
+         * @body For some reason, it gives poster link in current scenario and also order of links are not correct.
+         */
+        if (videos[j] && videos[j].src) {
+          dlink = videos[j].src
+          label = 'Download Video'
+        }
+        j += 1
+      }
+
+      // console.log(dlink)
 
       dlbutton.innerHTML = `
-                <a download href=${dlink}>
-                <button class="instanshu-unite instanshu-sm instanshu-success">Download</button>`;
+                <a download href=${escapeHTML(dlink)}>
+                <button class="instanshu-unite instanshu-sm instanshu-success">${escapeHTML(
+    label
+  )}</button></a>`
 
       pfbutton.innerHTML = `
-                <a download href=${dplink}>
+                <a download href=${escapeHTML(dplink)}>
                 <button class="instaashu-material-circle instaashu-sm
                 instaashu-success instaashu-no-outline"
                 style ="margin-right:10px; padding: 4px 1px;">DP<i class="fas fa-arrow-circle-down"></i>
-                </button>`;
+                </button></a>`
 
-      if(headers[i].querySelectorAll('button').length < 2) {
-        headers[i].appendChild(pfbutton);
-        headers[i].appendChild(dlbutton);
+      buttonsParent.appendChild(pfbutton)
+      buttonsParent.appendChild(dlbutton)
+
+      if (headers[i].querySelectorAll('button').length < 2) {
+        headers[i].appendChild(buttonsParent)
       }
-      btns = headers[i].querySelectorAll('button');
-      if(btns[1] && btns[1].parentElement.href === document.location.href) {
-        btns[1].parentElement.href = dlink;
+      const btns = headers[i].querySelectorAll('button')
+      if (btns[1] && btns[1].parentElement.href === document.location.href) {
+        btns[1].parentElement.href = dlink
       }
     }
   }
-  window.onscroll = function() {refresh()};
+  window.onscroll = function ref () {
+    refresh()
+  }
 }
 
-function refresh() {
+function refresh () {
   // as we only can have 8 articles at a time.
-      injectButtons();
+  injectButtons()
 }
 
-injectButtons();
+injectButtons()
