21e40ea9ec5d77ec094386af1400c70203e5ad0c
Rahul Gurung <gurungrahul2@gmail.com>
2019-01-02 10:01:48 +0530

    #8 - added Download All Feature (#56)
    * #8 - added Download All button
    
    * lint fixes
    
    * converted pics into zip
    
    * feedback - I
    
    * used boolean instead of int
    * aligned button to remove collision
    * used image(num).jpg format

--- background.js
+++ background.js
@@ -3,6 +3,9 @@ chrome.webNavigation.onCompleted.addListener(
     let tabId = navigationEvent.tabId
     chrome.tabs.insertCSS(tabId, { file: 'insta-down-css.css' })
     chrome.tabs.executeScript(tabId, { file: 'download.js' })
+    chrome.tabs.executeScript(tabId, { file: 'dist/jszip.min.js' })
+    chrome.tabs.executeScript(tabId, { file: 'dist/jszip-utils.min.js' })
+    chrome.tabs.executeScript(tabId, { file: 'dist/FileSaver.min.js' })
   },
   { url: [{ hostSuffix: '.instagram.com' }] }
 )
